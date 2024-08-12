a90a2f3299f7a9f356eb80f208eced3301e9f2d2
DmitriiTMS <dmitrii12_88@mail.ru>
2024-08-12 10:40:42 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const resultArr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const [first, end] = bracketsConfig.find((item) => item.includes(str[i]));
+
+    if (str[i] === first && !resultArr.includes(first)) {
+      resultArr.push(end);
+      continue;
+    }
+
+    const bracketArr = resultArr.pop();
+    if (str[i] === bracketArr) {
+      continue;
+    }
+    return false;
+  }
+
+  return !resultArr.length;
 }
